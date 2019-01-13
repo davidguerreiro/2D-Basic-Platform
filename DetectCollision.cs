96da9cbe96b384e9661ec5f1a1f50c944f6b116d
davidguerreiro <me@davidguerreiro.com>
2019-01-13 19:53:24 +0000

    add collision2D script to player to collect coins

---
+++ DetectCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Check collisons with other objects
+    void OnCollisionEnter2D( Collision2D coll ) {
+        // get tag from the GameObject we have just collided.
+        GameObject objectCollided = coll.collider.gameObject;
+
+        // check if we are colliding a coin gameobject.
+        if ( objectCollided.tag == "pick_me" ) {
+            // destroy the coin.
+            Destroy( objectCollided );
+        }
+    }
+}
