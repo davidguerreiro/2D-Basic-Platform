d24851026ad88f6b8ef07db6f458fcf90bd84da3
davidguerreiro <me@davidguerreiro.com>
2019-01-19 18:35:43 +0000

    add script to createn softBlock logic

---
+++ SoftBlockLogic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoftBlockLogic : MonoBehaviour
+{
+
+    public int hardness = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void blockIsHit() {
+        // TODO - Display animation.
+        // TODO - Show children gameobjects.
+        hardness--;
+
+        if ( hardness <= 0 ) {
+            Destroy( gameObject );
+        }
+    }
+}
