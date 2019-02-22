0f73ef45b3504c5749efa637b228d5ca79c788bb
davidguerreiro <me@davidguerreiro.com>
2019-02-22 19:59:55 +0000

    add load level 2 when collecting the final emerald

--- EndLevel.cs
+++ EndLevel.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndLevel : MonoBehaviour
 {   
     void OnCollisionEnter2D(Collision2D other)
     {
         if ( other.gameObject.tag == "Player" ) {
-            // TODO: Call level completed function here.
-            print( "Level completed !!" );
+            if ( SceneManager.GetActiveScene().name == "Level1" ) {
+                Destroy( gameObject );
+                SceneManager.LoadScene("Level2");
+            }
         }
     }
 }
