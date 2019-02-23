1bf62e226928509b1731507914344d095b569e85
davidguerreiro <me@davidguerreiro.com>
2019-02-23 19:39:05 +0000

    add load level function to UI controller

---
+++ UI/ControlButtons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Load a new scene. Use this method to load new levels.
+    /// </summary?
+    /// <param name="levelName">String - Name of the scene to be loaded</param>
+    public void startLevel( string levelName ) {
+        if ( levelName != "" ) {
+            SceneManager.LoadScene( levelName );
+        } else {
+            Debug.Log( "Scene cannot be loaded - Invalid Scene name" );
+        }
+    }
+}
