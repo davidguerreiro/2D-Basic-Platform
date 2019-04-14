43644defd43f2f7f15e9d9a3d6581f8dd3db2b66
davidguerreiro <me@davidguerreiro.com>
2019-04-14 13:26:28 +0100

    complete main screen animations

--- UI/pressSpaceBarAnimation.cs
+++ UI/pressSpaceBarAnimation.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pressSpaceBarAnimation : MonoBehaviour
 {
+    public float baseDuration = 1.0f;                   // Base animation duration - Base animation is the standard animation displayed before the user press the space bar button.
+    public float transitionDuration = 1.5f;             // Transition animation duration - Transition animation is the animation displayed when the user press the space bar but before the Main Menu is displayed.
+    private Text text;                                  // Text componentent.
+    private Color colorDisplayed;                       // Color used when the text is displayed.
+    private Color colorHidden;                          // Color used when the text is hidden.
+    private bool displayBaseAnimation;                  // Whether the base animation is being displayed.
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ( displayBaseAnimation ) {
+            StartCoroutine( DisplayBaseAnimation() );
+        }        
+    }
+
+    /// <summary>
+    /// Press Start Init class.
+    /// </summary>
+    public void Init() {
+        text = gameObject.GetComponent<Text>();
+        colorDisplayed = new Color( text.color.r, text.color.g, text.color.b, 1f );
+        colorHidden = new Color( text.color.r, text.color.g, text.color.b, 0f );
+
+        displayBaseAnimation = true;
+    }
+
+    /// <summary>
+    /// Display press start text base animation.
+    /// </summary>
+    public IEnumerator DisplayBaseAnimation() {
+        displayBaseAnimation = false;
+
+        if ( text.color.a == 1f ) {
+            text.color = colorHidden;
+            yield return new WaitForSeconds( baseDuration );
+        } else {
+            text.color = colorDisplayed;
+            yield return new WaitForSeconds( baseDuration );
+        }
+
+        displayBaseAnimation = true;
     }
 }
---
+++ UI/titleScreenController.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class titleScreenController : MonoBehaviour
+{
+    public GameObject backgroundImage;                  // Main title background image gameObject.
+    public GameObject gameTitle;                        // Main title text gameObject.
+    public GameObject developerText;                    // Developer text.
+    public GameObject mainMenuPanel;                    // Main menu panel gameObject.
+    public GameObject pressStartText;                   // Press start text gameObject.
+    public GameObject sceneCover;                       // Black image used to transition from main title scene to other scenes.
+    private float toMoveTitle = - 479f;                 // Where to move the title in the X axis during the animation.
+    private float fadeBackSpeed = 1f;                  // Background fade in animation.
+    private float fadeTextSpeed = 1.5f;                  // Screen text fade in speed;
+    private float moveText = 200f;                       // Move title animation speed;
+    private bool animateBackGround = false;             // Wheter to start the background animation;
+    private bool animateTitle = false;                  // Wheter to start the title animation.
+    private bool displayOtherTexts = false;             // Wheter to display the press space bar text and the developer text.
+    private bool mainAnimationsCompleted = false;       // Wheter all the animations for main title screen have been completed.
+    private RawImage backgroundImageComp;               // Background image component.
+    private Text gameTitleText;                         // Game Title Text component.
+    private Text devText;                               // Developer Text component.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InitCoroutines();
+    }
+
+    /// <summary>
+    /// Init title screen controller.
+    /// </summary>
+    private void Init() {
+        // get components from UI gameobjects.
+        backgroundImageComp = backgroundImage.GetComponent<RawImage>();
+        gameTitleText = gameTitle.GetComponent<Text>();
+        devText = developerText.GetComponent<Text>();
+
+        // trigger first animation - the background image.
+        animateBackGround = true;
+    }
+
+    /// <summary>
+    /// Display animations and init coroutines
+    /// This method is only called in Update.
+    /// </summary>
+    private void InitCoroutines() {
+        if ( animateBackGround ) {
+            StartCoroutine( displayBackgroundImage() );
+        }
+
+        if ( animateTitle ) {
+            StartCoroutine( displayTitle() );
+        }
+
+        if ( displayOtherTexts ) {
+            StartCoroutine( displayAllOtherElements() );
+        }
+    }
+
+    /// <summary>
+    /// Fade in background image.
+    /// </summary>
+    private IEnumerator displayBackgroundImage() {
+        bool internalFlag = true;                   // Used to avoid triggeing the title animation several times during the fade in.
+        animateBackGround = false;
+        yield return new WaitForSeconds( 2f );
+
+        while ( backgroundImageComp.color.a < 1f ) {
+            backgroundImageComp.color = new Color( backgroundImageComp.color.r, backgroundImageComp.color.g, backgroundImageComp.color.b, backgroundImageComp.color.a + ( fadeBackSpeed * Time.deltaTime ) );
+
+            yield return new WaitForSeconds( 0.1f );
+
+            // show title before the animation is totally completed.
+            if ( backgroundImageComp.color.a > 0.6f && internalFlag == true ) {
+                animateTitle = true;
+                internalFlag = false;
+            }
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// Main title screen title animation.
+    /// </summary>
+    private IEnumerator displayTitle() {
+        animateTitle = false;
+
+        // fade in text.
+        while( gameTitleText.color.a < 1f ) {
+            gameTitleText.color = new Color( gameTitleText.color.r, gameTitleText.color.g, gameTitleText.color.b, gameTitleText.color.a + ( fadeTextSpeed * Time.deltaTime ) );
+            yield return null;
+        }
+
+        yield return new WaitForSeconds( 1f );
+
+        // move title to the right.
+        while ( gameTitle.transform.localPosition.x > toMoveTitle ) {
+            gameTitle.transform.localPosition = new Vector3( gameTitle.transform.localPosition.x - ( moveText * Time.deltaTime ), gameTitle.transform.localPosition.y, gameTitle.transform.localPosition.z );
+            yield return null;
+        }
+
+        displayOtherTexts = true;
+    }
+
+    /// <summary>
+    /// Display press spacebar text and developer name
+    /// text.
+    /// </summary>
+    private IEnumerator displayAllOtherElements() {
+        displayOtherTexts = false;
+
+        // display press space bar text.
+        pressStartText.SetActive( true );
+
+        // fade in dev text.
+        while ( devText.color.a < 1f ) {
+            devText.color = new Color( devText.color.r, devText.color.g, devText.color.b, devText.color.a + ( fadeTextSpeed * Time.deltaTime ) );
+            yield return null;
+        }
+    }
+}
